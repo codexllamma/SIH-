057a67485fa8c50584d83bbeeb8d1af4b3f81dfe
codexllamma <codexllamma@gmail.com>
2025-09-01 10:57:17 +0530

    Initial Konva Setup Done

---
+++ rail-ai/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TrainSimulation from "./components/train-simulation";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Train Simulation Demo</h1>
+      <TrainSimulation />
+    </div>
+  );
+}
+
+export default App
+
